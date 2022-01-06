37a78f8a067c1d4b962203457b9f4dce566ccf41
Adel-abdelfattah <90777666+Adel-abdelfattah@users.noreply.github.com>
2022-01-06 17:29:43 +0200

    fsfsfs

--- js/custom.js
+++ js/custom.js
@@ -49,19 +49,37 @@ drawProducts();
 
 
 let addedItem = localStorage.getItem('productsInCart') ? JSON.parse(localStorage.getItem('productsInCart')) : [];
-if (addedItem) {
+if (addedItem.length > 0) {
         addedItem.map(item => {
         cartDom.innerHTML += `
-        <div class="cart-item row px-5 mb-2 justify-content-between">
-            <img src="${item.imgUrl}"
-            <p class="">${item.title}</p>
-            <p>${item.qty}</p>
+        <div class="cart-item col-12 my-2 border-bottom py-2">
+            <div class="row justify-content-start align-items-center">
+                <div class="">
+                    <img src="${item.imgUrl}">
+                </div>
+                <div class="col flex-grow-1 ">
+                    <div class=" justify-content-start align-items-center row flex-grow-1">
+                        <h3 class="col-12  mb-1  mx-3 mx-lg-auto">${item.title}</h3>
+                        <p class="col-12  my-1  mx-3 mx-lg-auto h6 text-success"><span class="pr-2 text-dark">price</span>$${item.price}</p>
+                        <div class="col-12  my-1  mx-3 mx-lg-auto d-flex qty align-items-center">
+                            <div class="dec mr-2"><i class="fa fa-minus badge-danger rounded-circle p-1 hvr-pop" onclick="decQty(${item.id})"></i></div>
+                            <div class="numb mx-2 text-success h6 mb-0">${item.qty}</div>
+                            <div class="dec mx-2"><i class="fa fa-plus badge-danger rounded-circle p-1 hvr-pop" onclick="incQty(${item.id})"></i></div>
+                        </div>
+                        <p class="col-12  my-1  mx-3 mx-lg-auto h6 pr-2 text-success"><span class="pr-2 text-dark">Total</span>$${item.price * item.qty}</p>
+                        <div class="  mx-3 my-1 ">
+                            <button class="add-cart btn btn-danger" onclick="removeFrmCart(${item.id})"><i class="fas fa-trash"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>`;
         cartSpanDom.style.display="block";
         cartSpanDom.innerHTML = addedItem.length;
-        
     });
     totalCost();
+}else {
+    cartDom.innerHTML = `<p class="text-danger text-center">There are no Products in your Cart</p>`;
 }
 
 // add to cart 
@@ -83,12 +101,29 @@ function addToCart(id) {
     cartDom.innerHTML = '';
     addedItem.forEach(item => {
         cartDom.innerHTML += `
-        <div class="cart-item row px-5 my-2 justify-content-between border-bottom border-1">
-            <img src="${item.imgUrl}"
-            <p class="">${item.title}</p>
-            <p>${item.qty}</p>
+        <div class="cart-item col-12 my-2 border-bottom py-2">
+            <div class="row justify-content-start align-items-center">
+                <div class="">
+                    <img src="${item.imgUrl}">
+                </div>
+                <div class="col flex-grow-1 ">
+                    <div class=" justify-content-start align-items-center row flex-grow-1">
+                        <h3 class="col-12  mb-1  mx-3 mx-lg-auto">${item.title}</h3>
+                        <p class="col-12  my-1  mx-3 mx-lg-auto h6 text-success"><span class="pr-2 text-dark">price</span>$${item.price}</p>
+                        <div class="col-12  my-1  mx-3 mx-lg-auto d-flex qty align-items-center">
+                            <div class="dec mr-2"><i class="fa fa-minus badge-danger rounded-circle p-1 hvr-pop" onclick="decQty(${item.id})"></i></div>
+                            <div class="numb mx-2 text-success h6 mb-0">${item.qty}</div>
+                            <div class="dec mx-2"><i class="fa fa-plus badge-danger rounded-circle p-1 hvr-pop" onclick="incQty(${item.id})"></i></div>
+                        </div>
+                        <p class="col-12  my-1  mx-3 mx-lg-auto h6 pr-2 text-success"><span class="pr-2 text-dark">Total</span>$${item.price * item.qty}</p>
+                        <div class="  mx-3 my-1 ">
+                            <button class="add-cart btn btn-danger" onclick="removeFrmCart(${item.id})"><i class="fas fa-trash"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-            ` ;
+        ` ;
     })
 
     
@@ -125,5 +160,142 @@ function totalCost() {
     `;
     } else {
         tCost.innerHTML = '';
+    }
+}
+
+function removeFrmCart (id) {
+    let productsInCart = localStorage.getItem('productsInCart');
+    let items = JSON.parse(productsInCart);
+     if (productsInCart) {
+        
+        let filteredItems = items.filter((item) => item.id !== id);
+        localStorage.setItem('productsInCart', JSON.stringify(filteredItems));
+        drawProducts(filteredItems); 
+
+
+        if (filteredItems.length > 0) {
+                cartDom.innerHTML = '';
+                filteredItems.forEach(item => {
+                cartDom.innerHTML += `
+                <div class="cart-item col-12 my-2 border-bottom py-2">
+            <div class="row justify-content-start align-items-center">
+                <div class="">
+                    <img src="${item.imgUrl}">
+                </div>
+                <div class="col flex-grow-1 ">
+                    <div class=" justify-content-start align-items-center row flex-grow-1">
+                        <h3 class="col-12  mb-1  mx-3 mx-lg-auto">${item.title}</h3>
+                        <p class="col-12  my-1  mx-3 mx-lg-auto h6 text-success"><span class="pr-2 text-dark">price</span>$${item.price}</p>
+                        <div class="col-12  my-1  mx-3 mx-lg-auto d-flex qty align-items-center">
+                            <div class="dec mr-2"><i class="fa fa-minus badge-danger rounded-circle p-1 hvr-pop" onclick="decQty(${item.id})"></i></div>
+                            <div class="numb mx-2 text-success h6 mb-0">${item.qty}</div>
+                            <div class="dec mx-2"><i class="fa fa-plus badge-danger rounded-circle p-1 hvr-pop" onclick="incQty(${item.id})"></i></div>
+                        </div>
+                        <p class="col-12  my-1  mx-3 mx-lg-auto h6 pr-2 text-success"><span class="pr-2 text-dark">Total</span>$${item.price * item.qty}</p>
+                        <div class="  mx-3 my-1 ">
+                            <button class="add-cart btn btn-danger" onclick="removeFrmCart(${item.id})"><i class="fas fa-trash"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+                    
+                cartSpanDom.style.display="block";
+                cartSpanDom.innerHTML = filteredItems.length;
+            })
+        } else {
+            cartSpanDom.style.display="none";
+            cartDom.innerHTML = `<p class="text-danger text-center">There are no Products in your Cart</p>`;
+        }
+    }
+}
+
+function incQty (id) {
+    let productsInCart = localStorage.getItem('productsInCart');
+    let items = JSON.parse(productsInCart);
+    let item = items.find((i) => i.id === id);
+    if (item) {
+        item.qty += 1;
+    }
+    localStorage.setItem('productsInCart', JSON.stringify(items));
+    drawProducts(items);
+    if (items.length > 0) {
+        cartDom.innerHTML = '';
+        items.forEach(item => {
+        cartDom.innerHTML += `
+        <div class="cart-item col-12 my-2 border-bottom py-2">
+            <div class="row justify-content-start align-items-center">
+                <div class="">
+                    <img src="${item.imgUrl}">
+                </div>
+                <div class="col flex-grow-1 ">
+                    <div class=" justify-content-start align-items-center row flex-grow-1">
+                        <h3 class="col-12  mb-1  mx-3 mx-lg-auto">${item.title}</h3>
+                        <p class="col-12  my-1  mx-3 mx-lg-auto h6 text-success"><span class="pr-2 text-dark">price</span>$${item.price}</p>
+                        <div class="col-12  my-1  mx-3 mx-lg-auto d-flex qty align-items-center">
+                            <div class="dec mr-2"><i class="fa fa-minus badge-danger rounded-circle p-1 hvr-pop" onclick="decQty(${item.id})"></i></div>
+                            <div class="numb mx-2 text-success h6 mb-0">${item.qty}</div>
+                            <div class="dec mx-2"><i class="fa fa-plus badge-danger rounded-circle p-1 hvr-pop" onclick="incQty(${item.id})"></i></div>
+                        </div>
+                        <p class="col-12  my-1  mx-3 mx-lg-auto h6 pr-2 text-success"><span class="pr-2 text-dark">Total</span>$${item.price * item.qty}</p>
+                        <div class="  mx-3 my-1 ">
+                            <button class="add-cart btn btn-danger" onclick="removeFrmCart(${item.id})"><i class="fas fa-trash"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            ` ;
+        cartSpanDom.style.display="block";
+        cartSpanDom.innerHTML = items.length;
+    })
+    } else {
+        cartSpanDom.style.display="none";
+        cartDom.innerHTML = '';
+    }  
+}
+
+function decQty (id) {
+    let productsInCart = localStorage.getItem('productsInCart');
+    let items = JSON.parse(productsInCart);
+    let item = items.find((i) => i.id === id);
+    if (item.qty > 1) {
+        item.qty -= 1; 
+    } 
+    localStorage.setItem('productsInCart', JSON.stringify(items));
+    drawProducts(items);
+    if (items.length > 0) {
+        cartDom.innerHTML = '';
+        items.forEach(item => {
+        cartDom.innerHTML += `
+        <div class="cart-item col-12 my-2 border-bottom py-2">
+            <div class="row justify-content-start align-items-center">
+                <div class="">
+                    <img src="${item.imgUrl}">
+                </div>
+                <div class="col flex-grow-1 ">
+                    <div class=" justify-content-start align-items-center row flex-grow-1">
+                        <h3 class="col-12  mb-1  mx-3 mx-lg-auto">${item.title}</h3>
+                        <p class="col-12  my-1  mx-3 mx-lg-auto h6 text-success"><span class="pr-2 text-dark">price</span>$${item.price}</p>
+                        <div class="col-12  my-1  mx-3 mx-lg-auto d-flex qty align-items-center">
+                            <div class="dec mr-2"><i class="fa fa-minus badge-danger rounded-circle p-1 hvr-pop" onclick="decQty(${item.id})"></i></div>
+                            <div class="numb mx-2 text-success h6 mb-0">${item.qty}</div>
+                            <div class="dec mx-2"><i class="fa fa-plus badge-danger rounded-circle p-1 hvr-pop" onclick="incQty(${item.id})"></i></div>
+                        </div>
+                        <p class="col-12  my-1  mx-3 mx-lg-auto h6 pr-2 text-success"><span class="pr-2 text-dark">Total</span>$${item.price * item.qty}</p>
+                        <div class="  mx-3 my-1 ">
+                            <button class="add-cart btn btn-danger" onclick="removeFrmCart(${item.id})"><i class="fas fa-trash"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            ` ;
+        cartSpanDom.style.display="block";
+        cartSpanDom.innerHTML = items.length;
+    })
+    } else {
+        cartSpanDom.style.display="none";
+        cartDom.innerHTML = '';
     }
 }
